e16b38b155d3646e3ef26cb19bc14b40a9732035
linpeiwei <linpeiwei@f-road.com.cn>
2019-05-20 15:24:32 +0800

    返回组件添加keep-alive激活时更新this.route信息

--- src/components/common/removeTag.vue
+++ src/components/common/removeTag.vue
@@ -20,18 +20,27 @@ export default {
             default: ''
         }
     },
+    activated () {
+        /**
+         * 未关闭详情页面，通过点击列表重新进入详情页面，更新route信息
+         */
+        this.setRoute()
+    },
     created () {
         /**
          * remove当前tag常见于新增，编辑请求响应成功后调用
          * 若在响应成功前切换tag，动态获取this.$route会获取当前已切换的tag，导致需要remove的tag删除失败，故此处先获取并赋值route
          */
-        let { name, query, params } = this.$route
-        this.route = { name, query, params }
+        this.setRoute()
     },
     methods: {
         ...mapMutations([
             'removeTag'
         ]),
+        setRoute () {
+            let { name, query, params } = this.$route
+            this.route = { name, query, params }
+        },
         handleRemove (to) {
             this.removeTag({ route: this.route, to: this.to || to })
         }
